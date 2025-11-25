fb81288912ee4f27e5f58ded852c100f05124351
guildartss <informatica@tab-bolivia.com>
2025-11-25 15:48:52 -0400

    25 nov

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config(); //necesita dotenv
+const express = require('express'); //necesita express
+const morgan = require('morgan'); //necesita morgan
+const app = express(); //le indica que se va trabajar con express
+// Middlewares 
+app.use(morgan('dev'));
+app.use(express.json());
+
+// TODO: Cargar Rutas (lo haremos en Clase 2) 
+
+// Healthcheck Endpoint (para probar) el estatus de servidor
+app.get('/api/v1/healthcheck', (req, res) => {
+  res.status(200).json({ status: 'ok', timestamp: new Date() });
+});
+const PORT = process.env.PORT || 8080; //si en env no esta el puerto entonces por defecto carga el 8080
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`)); //levanta el servidor en el puerto indicado
